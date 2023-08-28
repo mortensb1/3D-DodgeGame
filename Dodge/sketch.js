ab22083d217ff97a77c570e3e2d5da781586cbe1
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2023-08-28 14:03:05 +0200

    Test for touch

--- Dodge/sketch.js
+++ Dodge/sketch.js
@@ -2,8 +2,12 @@ let camPos = [0,0,0];
 let camView = [0,0,0];
 let boxPos = [0,0,1200];
 let x = 0;
-let speed = 8
+let speed = 20
 let tuch = false;
+
+let distance;
+
+let test = 0;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
@@ -12,10 +16,37 @@ function setup() {
 
 function draw() {
   background(220);
-  console.log(tuch);
 
-  box(50,50,50);
+  Movement();
+  BoxMovement();
 
+  distance = sqrt((boxPos[0] - camPos[0])**2 + (boxPos[1] - camPos[1])**2 + (boxPos[2] - (camPos[2]-400))**2)
+  console.log(distance);
+
+  if(distance < 100)
+  {
+    test = true;
+  }
+
+}
+
+function BoxMovement()
+{
+  if(boxPos[2] < -401) 
+  {
+    boxPos[2] = 1200;
+    boxPos[0] = random(camPos[0] - 100, 100  + camPos[0])
+    boxPos[1] = random(camPos[1] - 100, camPos[1] + 100)
+  }
+    
+  translate(boxPos[0],boxPos[1],(boxPos[2]))
+  box(100);
+
+  boxPos[2] -= speed;
+}
+
+function Movement()
+{
   if(keyIsDown(39))       //rArrow
   {
     camPos[0] -= 2;
@@ -35,23 +66,4 @@ function draw() {
   }
 
   camera(camPos[0], camPos[1], camPos[2]-400, camView[0] + camPos[0], camView[1] + camPos[1], camView[2] + camPos[2], 0,1,0);
-
-
-  if(boxPos[2] < -401) 
-  {
-    boxPos[2] = 1200;
-    boxPos[0] = random(-100,100)
-    boxPos[1] = random(-100,100)
-  }
-    
-  translate(boxPos[0],boxPos[1],(boxPos[2]))
-  box(100);
-
-  boxPos[2] -= speed;
-
-  if(boxPos[0] - camPos[0] < 50 && boxPos[0] - camPos[0] > 50 && boxPos[1] - camPos[1] < 50 && boxPos[1] - camPos[1] > 50)
-  {
-    fill(255,0,0);
-  }
-
 }
